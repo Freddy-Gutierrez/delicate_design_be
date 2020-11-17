406aca7977db3b3477df690af71d0f94865b197b
Freddy Gutierrez <33851899+Freddy-Gutierrez@users.noreply.github.com>
2020-11-16 20:59:57 -0800

    prep for hosting

--- startup/prod.js
+++ startup/prod.js
@@ -3,6 +3,5 @@ const compression = require("compression");
 
 module.exports = function (app) {
   app.use(helmet());
-  app.use(compression());
-  app.use(cors({ exposedHeaders: "x-auth-token" }));
+  app.use(compression());  
 };
