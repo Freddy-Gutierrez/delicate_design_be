e9b8a54c845cdd98604e7cc5633d07378dfc715a
Freddy Gutierrez <33851899+Freddy-Gutierrez@users.noreply.github.com>
2020-11-16 21:05:06 -0800

    prep for hosting

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,7 @@ const validate = require("../middleware/validate");
 const { Product } = require('../models/product');
 
 
+
 router.get('/', async(req, res) => {
     const products = await Product.find();
     res.status(200).send(products);
